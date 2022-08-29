5b20b7d955535388ba25f1d1d04dc672a86f900d
Aleksandr Raskulov <alexandr1991r@gmail.com>
2022-08-29 15:42:13 +0300

    alfa-version-commit
    Desktop version landscape done
    Modal done

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,37 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
-  };
+const backdrop = document.querySelector('.backdrop');
+const modal = document.querySelector('.modal');
+const burger = document.querySelector('.burger');
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+const closeIcon = document.querySelector('.close-icon');
 
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
+closeIcon.addEventListener('click', handlerIconClose);
+
+function handlerIconClose() {
+  onCloseModal();
+}
+
+burger.addEventListener('click', handlerClick);
+
+function handlerClick() {
+  backdrop.classList.remove('is-hidden');
+  backdrop.addEventListener('click', handlerBackDrop);
+  window.addEventListener('keydown', handlerKeyDown);
+}
+
+function handlerBackDrop(evt) {
+  if (evt.target === evt.currentTarget) {
+    onCloseModal();
   }
-})();
+}
+
+function handlerKeyDown(evt) {
+  if (evt.keyCode === 27) {
+    onCloseModal();
+  }
+}
+
+function onCloseModal() {
+  backdrop.classList.add('is-hidden');
+  backdrop.removeEventListener('click', handlerBackDrop);
+  window.removeEventListener('keydown', handlerKeyDown);
+}
