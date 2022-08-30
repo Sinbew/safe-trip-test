a3a34114709b56888cba7aab702d8ef1aa3c331c
Aleksandr Raskulov <alexandr1991r@gmail.com>
2022-08-30 11:13:12 +0300

    Connected babel

--- js/modal-control.js
+++ js/modal-control.js
@@ -13,16 +13,20 @@ textArea.setAttribute('disabled', 'true');
 
 function handlerInput(evt) {
   int = +evt.target.value;
-
+  if (int <= 0) {
+    evt.target.value = '';
+  }
   if (int) {
     textArea.removeAttribute('disabled', 'false');
   } else {
     textArea.setAttribute('disabled', 'true');
   }
 
-  textArea.setAttribute('maxLength', int);
   setTimeout(() => {
-    textArea.value = textArea.value.slice(0, int);
+    textArea.value = textArea.value
+      .split(' ')
+      .slice(0, int + countIgnoredWords(textArea.value.split(' ')))
+      .join(' ');
   }, 1000);
 }
 
@@ -34,4 +38,18 @@ function onlyLetters(evt) {
   } else {
     evt.preventDefault();
   }
+}
+
+textArea.addEventListener('input', textAreaInput);
+
+function textAreaInput(evt) {
+  const res = evt.target.value.split(' ');
+
+  if (res.length === int + countIgnoredWords(res) + 1) {
+    evt.target.setAttribute('disabled', 'true');
+  }
+}
+
+function countIgnoredWords(words) {
+  return words.filter(el => el === 'a' || el === 'an' || el === 'the').length;
 }
